86f4ebff187bdb903f3d93770f3c9f02d6bdb2fc
weiser <weiser@telysta.com>
2025-02-20 06:44:10 +0800

    D:/Code/Git/

--- telystaback/telystaback/settings.py
+++ telystaback/telystaback/settings.py
@@ -18,7 +18,7 @@ import environ
 env = environ.Env()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
-environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
+environ.Env.read_env(os.path.join(BASE_DIR, '.env.development'))
 
 
 # Quick-start development settings - unsuitable for production
