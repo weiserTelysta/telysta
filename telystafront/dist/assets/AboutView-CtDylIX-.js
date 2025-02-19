c0a19fb9d4a97f5a20d1d4e1a14b1a00d18036ae
weiser <weiser@telysta.com>
2025-02-20 06:31:07 +0800

    adjust docker

---
+++ telystafront/dist/assets/AboutView-CtDylIX-.js
@@ -0,0 +1,1 @@
+import{_ as t}from"./index-CoVPq6PE.js";const e={};function r(c,_){return" test "}const o=t(e,[["render",r]]);export{o as default};
---
+++ telystafront/dist/assets/ActivationView-9766KbtV.js
@@ -0,0 +1,1 @@
+import{_ as e,c,o as t}from"./index-CoVPq6PE.js";const o={};function n(r,a){return t(),c("div")}const _=e(o,[["render",n]]);export{_ as default};
