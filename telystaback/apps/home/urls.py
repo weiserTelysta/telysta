25e9e52eb5e570afb2574528356527cad9323920
weiser <weiser@telysta.com>
2025-02-20 13:41:05 +0800

    docker adjust

--- telystaback/apps/home/urls.py
+++ telystaback/apps/home/urls.py
@@ -6,3 +6,11 @@ app_name = 'home'
 urlpatterns = [
     path('health/', views.HealthCheckView.as_view(), name='health'),
 ]
+from django.urls import path
+from . import views
+
+app_name = 'home'
+
+urlpatterns = [
+    path('health/', views.HealthCheckView.as_view(), name='health'),
+]
