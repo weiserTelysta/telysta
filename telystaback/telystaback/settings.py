a90730b05be2ce505b1a4cbd61c06d0448e8d005
weiser <weiser@telysta.com>
2025-02-20 23:55:15 +0800

    https-> http

--- telystaback/telystaback/settings.py
+++ telystaback/telystaback/settings.py
@@ -78,7 +78,7 @@ MIDDLEWARE = [
 
 # 跨域配置
 CORS_ALLOW_ALL_ORIGINS = False
-CORS_ALLOWED_ORIGINS = ["https://telysta.com","https://www.telysta.com","https://localhost","https://120.76.217.45"]
+CORS_ALLOWED_ORIGINS = ["http://telysta.com","http://www.telysta.com","http://localhost","http://120.76.217.45"]
 
 # JWT配置
 SIMPLE_JWT = {
